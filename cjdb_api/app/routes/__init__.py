24f90e74f2f64b316d02314167c289113c4c0d16
Cynthia Cai <81803109+cynthiacai56@users.noreply.github.com>
2022-11-28 21:40:25 +0100

    master version routes

--- cjdb_api/app/routes/__init__.py
+++ cjdb_api/app/routes/__init__.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 import cjdb_api.app.resources.querying as query
 
 master_blueprint = Blueprint("master", __name__)
-master_api = Api(coll_blueprint)
+master_api = Api(master_blueprint)
 
 # routes
-coll_api.add_resource(query.Item, "/")
+master_api.add_resource(query.Item, "/")
