4185b288f3f80e991926f917fe31ac42a265a604
Cynthia Cai <81803109+cynthiacai56@users.noreply.github.com>
2022-11-28 21:41:34 +0100

    Master and beta

--- cjdb_api/app/__init__.py
+++ cjdb_api/app/__init__.py
@@ -1,7 +1,8 @@
 from cjdb_api.app.vars import DEBUG, CONN_STRING
 from cjdb_api.app.db import db
 from cjdb_api.app.ma import ma
-from cjdb_api.app.routes import api_blueprint
+from cjdb_api.app.routes import master_blueprint
+from cjdb_api.app.routes.beta_routes import beta_blueprint
 from flask import Flask
 
 
@@ -14,7 +15,8 @@ def make_app():
     db.init_app(flask_app)
     ma.init_app(flask_app)
 
-    flask_app.register_blueprint(api_blueprint, url_prefix="/api")
+    flask_app.register_blueprint(master_blueprint, url_prefix="/collections")
+    flask_app.register_blueprint(beta_blueprint, url_prefix="/beta")
 
     return flask_app
 
